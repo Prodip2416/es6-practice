2777c6c6c6a597858a887fb8e9312f5157e31f68
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-06 23:43:21 +0600

    double vs triple equal in js

---
+++ advanced-js/double-vs-triple-equal.js
@@ -0,0 +1,11 @@
+let value1 = 0;
+let value2 = false;
+
+if(value1 == value2){
+    console.log('Condition is True.');
+}else{
+    console.log('Condition is False.');
+}
+
+// == Check only Value
+// === Check Value and Type
