2a687d37541652a780f309ea2855bb0d7f583da9
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-06 22:39:32 +0600

    true false find value

---
+++ advanced-js/true-false.js
@@ -0,0 +1,21 @@
+const name = '0';
+
+if(name){
+    console.log('Condition is True.');    
+}else{
+    console.log('Condition is False.');
+}
+
+// False Value...
+// ''
+// 0
+// undefined
+// null
+// NaN
+// false
+
+// True Value...
+// []
+// {}
+// true
+// '0'
