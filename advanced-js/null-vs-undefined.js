ce28376b06e68fdb75e7b56af20f9c1e090d3547
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-06 22:58:52 +0600

    null vs undefined

---
+++ advanced-js/null-vs-undefined.js
@@ -0,0 +1,14 @@
+function Add(num,num1){
+    console.log(num,num1);
+}
+let result = Add(12);
+console.log(result);
+
+let arr = [1,2,4,5];
+console.log(arr[9]);
+
+let obj = {name:"pro", email:"pro@gmail.com"};
+console.log(obj.phone);
+
+// Way to find Null
+// When value is empty,,,,to set this variable is null
