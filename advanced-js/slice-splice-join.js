4f08ea534746234856535347dd25a881b1231d9a
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-07 20:34:13 +0600

    Array slice, splice, array join elements

---
+++ advanced-js/slice-splice-join.js
@@ -0,0 +1,5 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+console.log(nums.slice(3,5)); // 4,5
+console.log(nums.splice(3,5)); // 4,5,6,7,8 remove from the given array
+console.log(nums.join(', ')); 
