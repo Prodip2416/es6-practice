f2565a2e6abdc2c6b32621b398baacdfd41b368e
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-07 20:01:47 +0600

    Scope, block scope, access outer scope variable

---
+++ advanced-js/scope-variable.js
@@ -0,0 +1,19 @@
+let bonus = 20;
+
+function sum(num1, num2) {
+    let total = num1 + num2 + bonus;
+   // console.log(total);
+    if(total >10){
+        var mood = 'Crazy';
+        // console.log(mood);
+    }
+    console.log(mood);
+    return total;
+}
+
+console.log(day);
+var day = 'Friday';
+const result = sum(12, 13);
+console.log(result);
+// console.log(bonus);
+
