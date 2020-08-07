956c228c1fccde69d82d06b684dc6c0d7959d4ba
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-07 15:29:11 +0600

    map vs forEach

--- advanced-js/map.js
+++ advanced-js/map.js
@@ -10,13 +10,21 @@ let numbers = [2, 3, 4, 5, 6, 7, 8];
 
 
 // Updated style for array looping
-const result = numbers.map(function(element, index, array){
-   // console.log(element, index, array)
-    return element * element;
-});
-console.log(result);
+// const result = numbers.map(function(element, index, array){
+//    // console.log(element, index, array)
+//     return element * element;
+// });
+// console.log(result);
 
 
 // Smart Style for array looping
 // const result = numbers.map(x => x * x);
 // console.log(result);
+
+
+// Others example same as forEach
+// function logArrayElements(element, index, array) {
+//     console.log('a[' + index + '] = ' + element + ' Array = '+ array);
+//   }
+  
+//   [2, 5, , 9].map(logArrayElements)
