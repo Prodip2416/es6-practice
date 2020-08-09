78045e446419a1fea521071981810858c8738f03
prodip2416 <prodip.sarker.cse@gmail.com>
2020-08-09 13:42:36 +0600

    Asynchronous Javascript setTimeout, setInterval

---
+++ object/setTimeOut.js
@@ -0,0 +1,14 @@
+
+console.log(1111);
+setTimeout(function(){
+    console.log('This function call after all of work doing than 3s after.');
+}, 3000);
+console.log(2222);
+setTimeout(function(){
+    console.log('This function call after all of work doing than 5s after.');
+}, 5000);
+console.log(3333);
+setTimeout(function(){
+    console.log('This function call after all of work doing than 7s after.');
+}, 7000);
+console.log(4444);
