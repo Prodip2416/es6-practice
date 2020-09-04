4619647a3c1a7e0e82c8400ad4c39d577571d99d
prodip2416 <prodip.sarker.cse@gmail.com>
2020-09-04 14:52:41 +0600

    closure in js

---
+++ advanced-js/closure-explore.js
@@ -0,0 +1,18 @@
+// var num1 = 5;
+// var Add = function  (){
+//     var num2 = 7;
+//     return function(){
+//         return num1 + num2;
+//     };
+// };
+// console.dir(Add());
+
+
+// function bankAccount(initialBalance){
+//     var balance = initialBalance;
+//     return function(){
+//         return balance;
+//     }
+// }
+
+// console.dir(bankAccount(10000));
