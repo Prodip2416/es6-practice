5914869b4d98457671aed30e24ed30237f5074ae
prodip2416 <prodip.sarker.cse@gmail.com>
2020-09-04 10:16:52 +0600

    hoisting in ja

---
+++ hoisting/hoisting.js
@@ -0,0 +1,20 @@
+
+
+// console.log(a);
+// var a;
+// a = 'Friday';
+
+// function myFunc(){
+//     language = "JS";
+//     var language;
+//     console.log(language);
+// }
+
+// myFunc();
+
+// myFunc();
+
+// const myFunc = function(){
+//      var language = "JS"
+//     console.log(language);
+// }
